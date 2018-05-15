38e151fdc2e1e2bb65ad22613f437b3780486642
Serghey <it.serghey@gmail.com>
2018-05-15 21:58:58 +0300

    Ошибки слияния веток
    Ошибки конфликтов веток

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { withStyles } from "material-ui/styles";
 
@@ -26,26 +25,3 @@ const App =({ classes }) => (
 );
 
 export default withStyles(styles)(App);
-=======
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
->>>>>>> master
