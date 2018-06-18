a3af7865ab610a684ad3f5c24e595ec92b152821
Serghey <it.serghey@gmail.com>
2018-06-18 15:06:08 +0300

    fix title-initials

--- src/utils/title-initials.js
+++ src/utils/title-initials.js
@@ -1,11 +1,14 @@
 export default function titleInitials(title) {
   try {
-    return title
-      .split(' ')
-      .map(word => word[0])
-      .map(char => char.toUpperCase())
-      .slice(0, 2)
-      .join('');
+    if (title) {
+      return title
+        .split(' ')
+        .map(word => word[0])
+        .map(char => char.toUpperCase())
+        .slice(0, 2)
+        .join('');
+    }
+    return '🐶';
   } catch (e) {
     // eslint-disable-next-line
     console.error(e);
